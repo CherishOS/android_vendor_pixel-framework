54f80a550ae81c100a3d9b1f32fa4ec602d83814
aswin7469 <aswinas@pixysos.com>
2023-06-17 19:42:46 +0530

    SystemUIGoogle: Include aosp only modules from ReferenceSystemUIModule
    * no need for batterysaver module as we already got google one
    
     https://github.com/PixysOS/frameworks_base/commit/a4ff24d4b3c0aac644504fa79dae7e416f41ade2
     https://github.com/PixysOS/frameworks_base/commit/e9e3ebac18c08f172a5b56449c895cab969a18dd
    
    Change-Id: I96134be5f60cdbd6555438c384e00b7546459964
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
@@ -69,8 +69,6 @@ import com.android.systemui.statusbar.phone.HeadsUpManagerPhone;
 import com.android.systemui.statusbar.phone.KeyguardBypassController;
 import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
 import com.android.systemui.statusbar.policy.AccessibilityManagerWrapper;
-import com.android.systemui.statusbar.policy.BatteryController;
-import com.android.systemui.statusbar.policy.BatteryControllerImpl;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedControllerImpl;
@@ -83,6 +81,10 @@ import com.android.systemui.statusbar.policy.SensorPrivacyControllerImpl;
 import com.android.systemui.volume.dagger.VolumeModule;
 
 import com.google.android.systemui.NotificationLockscreenUserManagerGoogle;
+
+import com.android.systemui.rotationlock.RotationLockModule;
+import com.android.systemui.statusbar.policy.AospPolicyModule;
+
 import com.google.android.systemui.assist.AssistManagerGoogle;
 import com.google.android.systemui.assist.dagger.AssistModule;
 import com.google.android.systemui.columbus.dagger.ColumbusModule;
@@ -127,6 +129,8 @@ import dagger.Lazy;
         ElmyraModule.class,
         ColumbusModule.class,
         StatusBarEventsModule.class,
+        AospPolicyModule.class,
+        RotationLockModule.class,
         CherishModule.class
 })
 public abstract class SystemUIGoogleModule {
@@ -234,34 +238,6 @@ public abstract class SystemUIGoogleModule {
 
     @Binds
     abstract DockManager bindDockManager(DockObserver dockObserver);
-
-    @Provides
-    @SysUISingleton
-    static BatteryController provideBatteryController(
-            Context context,
-            EnhancedEstimates enhancedEstimates,
-            PowerManager powerManager,
-            BroadcastDispatcher broadcastDispatcher,
-            DemoModeController demoModeController,
-            DumpManager dumpManager,
-            @Main Handler mainHandler,
-            @Background Handler bgHandler,
-            UserContentResolverProvider userContentResolverProvider,
-            ReverseChargingController reverseChargingController) {
-        BatteryController bC = new BatteryControllerImplGoogle(
-                context,
-                enhancedEstimates,
-                powerManager,
-                broadcastDispatcher,
-                demoModeController,
-                dumpManager,
-                mainHandler,
-                bgHandler,
-                userContentResolverProvider,
-                reverseChargingController);
-        bC.init();
-        return bC;
-    }
 
     /** */
     @Binds
